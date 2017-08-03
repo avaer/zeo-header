3d6b6c78646699d1f8575df8ebafed99e84b965e
Adrian Biedrzycki <a@modules.io>
2017-08-03 14:12:37 -0700

    Initial commit

---
+++ js/header.js
@@ -0,0 +1,105 @@
+const home = document.currentScript.getAttribute('home');
+const tabsAttribute = document.currentScript.getAttribute('tabs');
+const urlsAttribute = document.currentScript.getAttribute('urls');
+const logoutAttribute = document.currentScript.getAttribute('logout');
+let urls = urlsAttribute.length > 0 ? urlsAttribute.split(',') : [];
+let tabs = tabsAttribute.length > 0 ? tabsAttribute.split(',').map((t, i) => ({
+  tab: t,
+  url: urls[i],
+})) : [];
+let tab = document.currentScript.getAttribute('tab');
+let username = document.currentScript.getAttribute('username');
+let headerEl = document.currentScript;
+
+const _capitalize = s => s[0].toUpperCase() + s.slice(1);
+const _nop = () => {};
+const _copyToClipboard = s => {
+  const mark = document.createElement('span');
+  mark.textContent = s;
+  mark.setAttribute('style', [
+    // reset user styles for span element
+    'all: unset',
+    // prevents scrolling to the end of the page
+    'position: fixed',
+    'top: 0',
+    'clip: rect(0, 0, 0, 0)',
+    // used to preserve spaces and line breaks
+    'white-space: pre',
+    // do not inherit user-select (it may be `none`)
+    '-webkit-user-select: text',
+    '-moz-user-select: text',
+    '-ms-user-select: text',
+    'user-select: text',
+  ].join(';'));
+  document.body.appendChild(mark);
+
+  const range = document.createRange();
+  range.selectNode(mark);
+
+  const selection = document.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(range);
+
+  const successful = document.execCommand('copy');
+  return successful;
+};
+const _render = () => {
+  const oldHeaderEl = headerEl;
+
+  headerEl = document.createElement('header');
+  headerEl.classList.add('header');
+  headerEl.innerHTML = `\
+    <div class=header-left>
+      <div class=logo-wrap>
+        <img src="/img/logo.svg" class=logo>
+      </div>
+      <a href="${home}" class=header-logo-anchor>zeo</a>
+    </div>
+    <navbar>
+      ${tabs.map(t => `<a href="${t.url}" class="blue ${t.tab === tab ? 'selected' : ''}">${_capitalize(t.tab)}</a>`).join('\n')}
+    </navbar>
+    <div class=header-right>
+      ${username ? `<navbar>
+        <a class="blue copyable" id=header-copy-button>
+          <span class=avatar>
+            <img src="${creaturejs.makeStaticCreature('user:' + username)}" class=avatar-image id=avatar-image />
+            <span id=avatar-username>${username}</span>
+          </span>
+          <span class=copy>Click to copy</span>
+          <span class=notification>...Copied!</span>
+        </a>
+        <a href="/id/login" class=button id=header-logout-button>Logout</a>
+      </navbar>` : ''}
+    </div>
+  `;
+
+  const headerCopyButton = headerEl.querySelector('#header-copy-button');
+  headerCopyButton.addEventListener('click', () => {
+    _copyToClipboard(username);
+
+    headerCopyButton.classList.add('copied');
+
+    setTimeout(() => {
+      headerCopyButton.classList.remove('copied');
+    }, 2000);
+  });
+
+  const headerLogoutButton = headerEl.querySelector('#header-logout-button');
+  if (headerLogoutButton) {
+    headerLogoutButton.addEventListener('click', e => {
+      header.onlogout(e);
+    });
+  }
+
+  oldHeaderEl.parentNode.replaceChild(headerEl, oldHeaderEl);
+};
+_render();
+
+const header = {
+  setTab(newTab) {
+    tab = newTab;
+    _render();
+  },
+  onlogout: _nop,
+};
+window.header = header;
